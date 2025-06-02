e2a1ea54360a2872fc7ca6f2fa61c0e94a8824e3
Michael Guzman <mgzmn4@gmail.com>
2025-06-01 21:46:25 -0500

    feat: initial work on color

---
+++ lib/src/themes/color_constants.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+const Color black = Color(0xFF000000);
+const Color white = Color(0xFFFFFFFF);
+
+const red100 = Color(0xff2d0709);
+const red90 = Color(0xff520408);
+const red80 = Color(0xff750e13);
+const red70 = Color(0xffa2191f);
+const red60 = Color(0xffda1e28);
+const red50 = Color(0xfffa4d56);
+const red40 = Color(0xffff8389);
+const red30 = Color(0xffffb3b8);
+const red20 = Color(0xffffd7d9);
+const red10 = Color(0xfffff1f1);
+
+const coolGray100 = Color(0xff121619);
+const coolGray90 = Color(0xff21272a);
+const coolGray80 = Color(0xff343a3f);
+const coolGray70 = Color(0xff4d5358);
+const coolGray60 = Color(0xff878d96);
+const coolGray50 = Color(0xff878d96);
+const coolGray40 = Color(0xffa2a9b0);
+const coolGray30 = Color(0xffc1c7cd);
+const coolGray20 = Color(0xffdde1E6);
+const coolGray10 = Color(0xfff2f4f8);
+
+const gray100 = Color(0xff161616);
+const gray90 = Color(0xff262626);
+const gray80 = Color(0xff393939);
+const gray70 = Color(0xff525252);
+const gray60 = Color(0Xff6f6f6f);
+const gray50 = Color(0Xff8d8d8d);
+const gray40 = Color(0Xffa8a8a8);
+const gray30 = Color(0Xffc6c6c6);
+const gray20 = Color(0Xffe0e0e0);
+const gray10 = Color(0Xfff4f4f4);
+
+const warmGray100 = Color(0xff171414);
+const warmGray90 = Color(0xff272525);
+const warmGray80 = Color(0xff3c3838);
+const warmGray70 = Color(0xff565151);
+const warmGray60 = Color(0xff726e6e);
+const warmGray50 = Color(0xff8f8b8b);
+const warmGray40 = Color(0xffada8a8);
+const warmGray30 = Color(0xffcac5c4);
+const warmGray20 = Color(0xffe5e0df);
+const warmGray10 = Color(0xfff7f3f2);
---
+++ lib/src/themes/colors.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CarbonThemeData {
+  // Taken from Carbon Design System
+
+  // Background & surface tokens
+  final Color background; // $background
+  final Color layer01; // $layer01
+  final Color layer02; // $layer02
+  final Color layer03; // $layer03
+  final Color overlay01; // $overlay01
+
+  // Text tokens
+  final Color textPrimary; // $text-primary
+  final Color textSecondary; // $text-secondary
+  final Color textHelper; // $text-helper
+  final Color textError; // $text-error
+  final Color textOnColor; // $text-on-color
+  final Color textDisabled; // $text-disabled
+
+  // Icon tokens
+
+  // Border tokens
+
+  // Interactive tokens
+
+  // Suport tokens
+
+  // Inverse tokens
+
+  // Focus tokens
+
+  // Placeholder tokens
+
+  // Field tokens
+}
